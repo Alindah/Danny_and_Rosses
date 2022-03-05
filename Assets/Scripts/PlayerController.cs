041107dbd4976285035d69c71f2d1eb1a3688c61
Alinda <Alindah@users.noreply.github.com>
2022-03-05 18:17:48 -0500

    Added horizontal player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("MOVEMENT")]
+    public float speed = 5.0f;
+    public Vector3 orientation;     // x = -1 if facing left, 1 if facing right
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // Get player inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        //verticalInput = Input.GetAxis("Vertical");
+
+        // Move the player in direction they pressed
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            float horizontalSpeed = Time.deltaTime * horizontalInput * speed;
+            float verticalSpeed = Time.deltaTime * verticalInput * speed;
+
+            // Flip player if facing wrong direction
+            if (horizontalSpeed * orientation.x < 0)
+                FlipPlayer();
+
+            transform.Translate(Vector3.right * horizontalSpeed);
+            //transform.Translate(Vector3.up * verticalSpeed);
+        }
+    }
+
+    private void FlipPlayer()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        orientation.x = -orientation.x;
+    }
+}
