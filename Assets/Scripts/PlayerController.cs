777d2ae61fefa376a19df12c7be7b523bd820637
Alinda <Alindah@users.noreply.github.com>
2022-03-10 10:12:30 -0500

    Climb ladders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,24 @@ public class PlayerController : MonoBehaviour
     [Header("MOVEMENT")]
     public Vector3 orientation = new Vector3(1, 1, 1);     // x = -1 if facing left, 1 if facing right
     public float speed = 5.0f;
+    public float climbSpeedPercent = 0.5f;
     public float jumpForce = 200.0f;
     public LayerMask groundLayer;
     public float xBoundary = 8.0f;
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody2D rb;
-
-    private const string TAG_PLATFORM = "Platform";
+    private float def_speed;
+    private float gravity;
+    private bool isOnLadder = false;
+    private const string TAG_LADDER = "Ladder";
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        def_speed = speed;
+        gravity = rb.gravityScale;
     }
 
     // Update is called once per frame
@@ -28,17 +33,25 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    // Handle player movement
     private void MovePlayer()
     {
         // Get player inputs
         horizontalInput = Input.GetAxis("Horizontal");
-        //verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Allow player to climb up and down ladders if within range
+        if (isOnLadder && Input.GetButton("Vertical"))
+        {
+            float verticalSpeed = Time.deltaTime * verticalInput * speed * climbSpeedPercent;
+            rb.velocity = new Vector2(0, 0);    // Reset velocity to prevent jumping onto ladder from altering speed
+            transform.Translate(Vector3.up * verticalSpeed);
+        }
 
         // Move the player in direction they pressed
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
             float horizontalSpeed = Time.deltaTime * horizontalInput * speed;
-            float verticalSpeed = Time.deltaTime * verticalInput * speed;
 
             // Flip player if facing wrong direction
             if (horizontalSpeed * orientation.x < 0)
@@ -50,10 +63,11 @@ public class PlayerController : MonoBehaviour
         }
 
         // Make player jump
-        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        if (IsGrounded() && Input.GetButtonDown("Jump") && !isOnLadder)
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
     }
 
+    // Flip player towards direction they are moving
     private void FlipPlayer()
     {
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -61,6 +75,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // https://kylewbanks.com/blog/unity-2d-checking-if-a-character-or-object-is-on-the-ground-using-raycasts
+    // Check if player is on the ground
     private bool IsGrounded()
     {
         Vector2 position = transform.position;
@@ -74,4 +89,24 @@ public class PlayerController : MonoBehaviour
 
         return false;
     }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // Check if player is on ladder
+        if (other.tag == TAG_LADDER)
+        {
+            isOnLadder = true;
+            rb.gravityScale = 0.0f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == TAG_LADDER)
+        {
+            isOnLadder = false;
+            rb.gravityScale = gravity;
+        }
+    }
+
 }
