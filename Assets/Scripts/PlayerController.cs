43b1489c4a2334a49d29ce330fa4f1295b191ed4
Alinda <Alindah@users.noreply.github.com>
2022-03-12 19:20:50 -0500

    Cleaned up ladder climbing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,13 +13,11 @@ public class PlayerController : MonoBehaviour
     public float xBoundary = 8.0f;
     private float horizontalInput;
     private float verticalInput;
+    public float ladderOffset = 0.66f;     // Percent of body of player we will move below the platform while on a ladder - used to avoid platform collision
     private Rigidbody2D rb;
     private Collider2D col;
     private float gravity;
     private bool isOnLadder = false;
-    private bool isAboveLadder = false;
-    private bool isAtBottomLadder = false;
-    private float ladderOffset = 0.66f;     // Percent of body of player we will move below the platform while on a ladder - used to avoid platform collision
     private const string TAG_LADDER = "Ladder";
 
     // Start is called before the first frame update
@@ -55,7 +53,7 @@ public class PlayerController : MonoBehaviour
         }
 
         // Move the player in direction they pressed
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        if (Input.GetButton("Horizontal"))
         {
             float horizontalSpeed = Time.deltaTime * horizontalInput * speed;
 
@@ -63,9 +61,9 @@ public class PlayerController : MonoBehaviour
             if (horizontalSpeed * orientation.x < 0)
                 FlipPlayer();
 
-            // Do not allow movement beyond boundary
-            if (Mathf.Abs(transform.position.x + horizontalSpeed) < Mathf.Abs(xBoundary))
-                transform.Translate(Vector3.right * horizontalSpeed);            
+            // Move in direction, but do not allow movement beyond boundary or on ladder
+            if (Mathf.Abs(transform.position.x + horizontalSpeed) < Mathf.Abs(xBoundary) && !isOnLadder)
+                transform.Translate(Vector3.right * horizontalSpeed);
         }
 
         // Make player jump
@@ -89,7 +87,8 @@ public class PlayerController : MonoBehaviour
         float distance = 0.75f;
 
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
-        
+        Debug.DrawRay(position, direction, Color.green);
+
         if (hit.collider != null)
             return true;
 
@@ -98,50 +97,42 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        // Check if player is on ladder
         if (other.tag == TAG_LADDER)
         {
-            if (other.GetType() == typeof(BoxCollider2D))
-            {
-                isAboveLadder = true;
-
-                // Teleport to bottom of box collider (top of ladder) and position slightly down for player model
-                if (Input.GetAxis("Vertical") < 0 && isAboveLadder)
-                {
-                    TeleportTo(new Vector2(other.bounds.center.x, other.bounds.min.y), new Vector2(0, -(col.bounds.max.y - col.bounds.min.y) * ladderOffset));
-                    isAboveLadder = false;
-                }
-                else if (Input.GetAxis("Vertical") > 0 && isOnLadder)
-                {
-                    TeleportTo(new Vector2(other.bounds.center.x, other.bounds.max.y), new Vector2(0, (col.bounds.max.y - col.bounds.min.y) * ladderOffset));
-                    isAboveLadder = true;
-                    isOnLadder = false;
-                }
-
-                return;
-            }
-
-            /*
-            // If player is on the ground, they are not on a ladder but can access
+            // Allow access to ladder if on the ground
             if (IsGrounded())
             {
-                isOnLadder = false;
-                isAtBottomLadder = true;
-                return;
-            }*/
-
-            isOnLadder = true;
-            isAboveLadder = false;
-            rb.gravityScale = 0.0f;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == TAG_LADDER)
-        {
-            isOnLadder = false;
-            rb.gravityScale = gravity;
+                // If above ladder, teleport to bottom of box collider (top of ladder) and position slightly down for player model when pressing down
+                if (Input.GetAxis("Vertical") < 0 && other.GetType() == typeof(BoxCollider2D))
+                {
+                    TeleportTo(new Vector2(other.bounds.center.x, other.bounds.min.y), new Vector2(0, -(col.bounds.max.y - col.bounds.min.y) * ladderOffset));
+                    isOnLadder = true;
+                    rb.gravityScale = 0.0f;
+                }
+                // Climb onto a ladder from the bottom
+                else if (Input.GetAxis("Vertical") > 0 && other.GetType() != typeof(BoxCollider2D))
+                {
+                    TeleportTo(new Vector2(other.bounds.center.x, transform.position.y), new Vector2(0, 0));
+                    isOnLadder = true;
+                    rb.gravityScale = 0.0f;
+                }
+                // Player has reached the ground
+                else if (Input.GetAxis("Vertical") < 0 && isOnLadder)
+                {
+                    isOnLadder = false;
+                    rb.gravityScale = gravity;
+                }
+            }
+            else if (isOnLadder)
+            {
+                // Teleport above ladder onto platform if at top rung
+                if (Input.GetAxis("Vertical") > 0 && other.GetType() == typeof(BoxCollider2D))
+                {
+                    TeleportTo(new Vector2(other.bounds.center.x, other.bounds.max.y), new Vector2(0, (col.bounds.max.y - col.bounds.min.y) * ladderOffset));
+                    isOnLadder = false;
+                    rb.gravityScale = gravity;
+                }
+            }
         }
     }
 
@@ -150,9 +141,8 @@ public class PlayerController : MonoBehaviour
     {
         // Do not allow current velocity to affect object after teleportation
         if (!keepVelocity)
-            rb.gravityScale = 0.0f;
+            rb.velocity = new Vector2(0, 0);
 
         transform.position = position + offset;
     }
-
 }
